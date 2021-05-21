b014129763ea8f889674bc1366e1ecc90acffdcb
tollt <helloimtellurate@gmail.com>
2021-05-21 15:45:05 -0400

    Create imptxt.js

---
+++ imptxt.js
@@ -0,0 +1,11 @@
+var txtFile = new XMLHttpRequest();
+txtFile.open("GET", "https://raw.githubusercontent.com/tollt/tollt.github.io/master/tat.txt", true);
+txtFile.onreadystatechange = function() {
+    if (txtFile.readyState === 4) {  // Makes sure the document is ready to parse.
+        if (txtFile.status === 200) {  // Makes sure it's found the file.
+            allText = txtFile.responseText;
+            document.getElementById("p").innerHTML = allText
+        }
+    }
+}
+txtFile.send(null);
