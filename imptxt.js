4cbbf4673c27f9f3a038221ca89cbc87ccee3e48
tollt <helloimtellurate@gmail.com>
2021-05-21 16:12:40 -0400

    Update imptxt.js

--- imptxt.js
+++ imptxt.js
@@ -4,7 +4,7 @@ txtFile.onreadystatechange = function() {
     if (txtFile.readyState === 4) {  // Makes sure the document is ready to parse.
         if (txtFile.status === 200) {  // Makes sure it's found the file.
             allText = txtFile.responseText;
-            document.getElementById("p").innerHTML = allText.replace("\n", "<br>")
+            document.getElementById("p").innerHTML = allText.replace(/\n/g, "<br>")
         }
     }
 }
