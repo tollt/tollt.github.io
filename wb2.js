35368717792c8fc19b29ee133ec89aa313dbd656
tollt <helloimtellurate@gmail.com>
2021-05-25 21:43:05 -0400

    Update wb2.js

--- wb2.js
+++ wb2.js
@@ -1,5 +1,7 @@
-$("#enterbox").keyup(function(event) {
-    if (event.keyCode === 13) {
+document.getElementById("enterbox")
+    .addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.key === 13) {
         var inputVal = document.getElementById("enterbox").innerText
         document.getElementById("wordentered.strong").innerText = inputVal
         document.getElementById("enterbox").innerText = ""
