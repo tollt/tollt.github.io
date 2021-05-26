6ae9e33909038fd05ce7b2c40821a994d95c518b
tollt <helloimtellurate@gmail.com>
2021-05-25 21:20:32 -0400

    Update wb2.js

--- wb2.js
+++ wb2.js
@@ -2,5 +2,6 @@ $(document).on("keypress", "input", function(e){
     if(e.which == 13){
         var inputVal = $(this).val();
         document.getElementById("wordentered.strong").innerText = inputVal
+        document.getElementById("enterbox").innerText = ""
     }
 });
